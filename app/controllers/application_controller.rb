d994aa91e6149e8435326586127d38d98586dd98
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-01-16 16:40:35 +0000

    初回

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #サインイン後の遷移先をAboutページへ指定
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  #サインアウト後の遷移先をAboutページへ指定
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  
+  
+  
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  
+  
+end
